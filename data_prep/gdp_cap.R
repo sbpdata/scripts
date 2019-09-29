dcb82aa49bdd1ebf0187aeb51cd2c9443c95843e
Soren Post <balchenpost@protonmail.com>
2019-09-29 23:00:36 +0200

    fixed data reading, joining by iso3c

--- data_prep/gdp_cap.R
+++ data_prep/gdp_cap.R
@@ -1,21 +1,27 @@
 # This file reads gdp / cap data from three sources (WDI, maddison, PWT), formats and saves it.
+
+library(here)
 library(tidyverse)
+library(countrycode)
 library(haven)
 
+## DATA SOURCES ##
 # pwt9 contains the penn world tables v9
 library(pwt9)
 # wdi contains api for wdi
 library(WDI)
-# maddison conatains the maddison database, updated 2018
-# install_github("expersso/maddison")
-library(maddison)
+# Maddison data is downloaded from:
+# https://www.rug.nl/ggdc/historicaldevelopment/maddison/releases/maddison-project-database-2018 
+
+maddison_path = "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2018.dta"
 
 ## OUTPUT PATH
-output_dir <- "../data/joined_gdp_cap.csv"
+output_dir <- here("../data/joined_gdp_cap.csv")
 
 ################
 ### WDI data ###
 ################
+
 # WDI data is from: https://databank.worldbank.org/source/world-development-indicators#
 # Measured in constant 2011 intl dollars (PPP)
 
@@ -25,36 +31,54 @@ wdi <- as_tibble(WDI(country = "all",
 		      start = NULL))
 
 wdi_tidy <- wdi %>%
-	select(country = iso3c, year, wdi_gdp_cap = NY.GDP.PCAP.PP.CD) %>%
-	mutate_if(is.factor, as.character)
+	select(isocode = iso3c, year, wdi_gdp_cap = NY.GDP.PCAP.PP.CD) %>%
+	mutate_if(is.factor, as.character) %>%
+	mutate(year = as.double(year))
 
 #####################
 ### Maddison data ###
 #####################
 
 # Maddison data is from: https://www.rug.nl/ggdc/historicaldevelopment/maddison/
-mad <- as_tibble(maddison)
+mad <- read_stata(maddison_path)
 
 # for computing relative growth rates, authors recommend the RGDPNApc
 mad_tidy <- mad %>%
-	select(country = iso3c, year, mad_gdp_cap = rgdpnapc)
+	select(isocode = countrycode, year, mad_gdp_cap = rgdpnapc)
 
 ################
 ### PWT data ###
 ################
 
 # pwt data is from https://www.rug.nl/ggdc/productivity/pwt
-pwt_tidy <- as_tibble(pwt9.1) %>%
-	select(country = isocode, year, rgdpe, pop) %>% 
+pwt <- as_tibble(pwt9.1)
+
+pwt_tidy <- pwt %>% 
+	select(isocode, year, rgdpe, pop) %>% 
 	mutate(pwt_gdp_cap = rgdpe / pop) %>% 
 	mutate_if(is.factor, as.character) %>%
-	select(country, year, pwt_gdp_cap)
+	select(isocode, year, pwt_gdp_cap)
 
 #######################
 ### join the frames ###
 #######################
 joined <- wdi_tidy %>%
-	left_join(mad_tidy) %>%
-	left_join(pwt_tidy) 
+	full_join(mad_tidy) %>%
+	full_join(pwt_tidy) 
+
+# standardise country-names by running am external matching 
+# function on the country variable and dropping all the non-matched 
+# observations. This removes entries like "Arab World".
+joined$iso_new <- countrycode(sourcevar = joined$isocode,
+	     origin = 'iso3c',
+	     destination = 'iso3c')
+
+joined_clean <- joined %>%
+	drop_na(iso_new)
+
+# Only notable dropped is Kosovo. Nort
+dropped_obs <- joined %>% 
+	filter(isocode %in% setdiff(unique(joined$isocode), unique(joined$iso_new))) %>% pull(isocode) %>%
+	unique()
 
 write_csv(joined, output_dir)
